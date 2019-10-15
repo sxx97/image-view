7b66cc827cc7bbf4a609a9b0a80b8107d5bc5254
isongqingzhao@163.com <isongqingzhao@163.com>
2019-10-15 09:40:21 +0800

    监听内网地址

--- main.go
+++ main.go
@@ -18,5 +18,5 @@ func startServe() {
 	app.Handle("GET", "/", func(ctx iris.Context) {
 		ctx.HTML("<h1>开发中...</h1>")
 	})
-	app.Run(iris.TLS("116.62.213.108:443", "mycreat.pem", "mykey.key"))
+	app.Run(iris.TLS("172.16.0.0:443", "mycreat.pem", "mykey.key"))
 }
