b918ced6b1edf86b3585a0b114a8af504be92683
isongqingzhao@163.com <isongqingzhao@163.com>
2019-10-15 16:32:32 +0800

    添加配置文件

--- main.go
+++ main.go
@@ -19,5 +19,10 @@ func startServe() {
 		ctx.HTML("<h1>开发中...</h1>")
 	})
 	//iris.TLS("172.16.47.62:80", "mycreat.pem", "mykey.key")
-	app.Run(iris.Addr("172.16.47.62:80"))
+	app.Get("/test", testRoute);
+	app.Run(iris.Addr("0.0.0.0:80"), iris.WithConfiguration(iris.TOML("./config/main.tml")))
+}
+
+func testRoute(ctx iris.Context) {
+	ctx.Writef("Hello from method: %s and path: %s", ctx.Method(), ctx.Path())
 }
