5a6f9ede7402614d8a55c2241ba7528c5ab3bce6
songqingzhao <isongxiaobai@gmail.com>
2019-11-06 23:33:49 +0800

    更新前端文件

---
+++ webapp/precache-manifest.e848c1124c3e204c54b7cbe62f38f12e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c6ef4490bffefb1ec6",
+    "url": "/css/app.1a8d8e4e.css"
+  },
+  {
+    "revision": "56706474d8e823196357",
+    "url": "/css/chunk-vendors.a26ddc98.css"
+  },
+  {
+    "revision": "5cc9336a378268807c453a3da3dc3ca2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69c6ef4490bffefb1ec6",
+    "url": "/js/app.d53956f4.js"
+  },
+  {
+    "revision": "56706474d8e823196357",
+    "url": "/js/chunk-vendors.9db71fc4.js"
+  },
+  {
+    "revision": "67e2e4dcd0f0693e03f8f17824b085ff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
